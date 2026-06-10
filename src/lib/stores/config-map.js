e3cb297661caa3142e0bcad8e31d1b8c2b0363fa
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2026-06-10 09:28:00 +0200

    Update map configuration

--- src/lib/stores/config-map.js
+++ src/lib/stores/config-map.js
@@ -1,37 +1,243 @@
 import { writable } from 'svelte/store';
-
 export const mapConfig = writable({
-	title: 'Which EU countries offer the best employment prospects?',
-	subtitle: 'Future employment growth in these countries in 2022-2035 is estimated at 5.6%.',
-	datasetType: 'values',
-	datasetUnit: 'percent',
-	percentRounded: false,
-	colourSchemeType: 'sequential',
-	colourScheme: 'red',
-	colourSchemeClasses: 5,
-	colourSchemeReverse: false,
-	overrideScaleValues: false,
-	colorBarFirstValue: undefined,
-	colorBarLastValue: undefined,
-	headlineAvailable: true,
-	subheadlineAvailable: true,
-	tooltipAvailable: true,
-	scaleBarAvailable: true,
-	legendAvailable: true,
-	textSourceAvailable: true,
-	textSourceDescription: 'Source',
-	textSource: '',
-	textNoteAvailable: true,
-	textNoteDescription: 'Note',
-	textNote: '',
-	textDataAccessAvailable: true,
-	linkDataAccessDescription: 'Access the data',
-	linkDataAccess: '',
-	legend1: 'No data available',
-	legend1Color: '#E0E0E0',
-	tooltipExtraInfoLabel: '',
-	parsedData: null,
-	data: ``,
-	customUnitLabelAvailable: false,
-	customUnitLabel: undefined
+  "title": "Where is blood scarcity in the EU?",
+  "subtitle": "In 2023, around 16.5 million whole blood units were collected across the EU27, with Germany reporting the highest number at 3.7 million, followed by Italy, France and Spain. Among countries with larger populations, Bulgaria and Finland recorded some of the lowest figures, both below 0.2 million units.",
+  "colourSchemeClasses": 7,
+  "data": "country,id,value,extraInfo,text_content\nAustria,AT,0.386,false,\nBelgium,BE,0.408,false,\nBulgaria,BG,0.172,false,\nCroatia,HR,0.190,false,\nCyprus,CY,0.071,false,\nCzechia,CZ,0.444,false,\nDenmark,DK,0.185,false,\nEstonia,EE,0.047,false,\nFinland,FI,0.178,false,\nFrance,FR,2.269,false,\nGermany,DE,3.672,false,\nGreece,EL,0.545,false,\nHungary,HU,0.355,false,\nIreland,IE,0.131,true,Data from 2022\nItaly,IT,2.564,false,\nLatvia,LV,0.060,false,\nLithuania,LT,0.108,false,\nLuxembourg,LU,0.018,true,Data from 2022\nMalta,MT,0.016,false,\nPoland,PL,1.358,false,\nPortugal,PT,0.300,false,\nRomania,RO,0.417,false,\nSlovak Republic,SK,0.228,false,\nSlovenia,SI,0.082,false,\nSpain,ES,1.565,false,\nSweden,SE,0.370,false,\nThe Netherlands,NL,0.385,false,",
+  "parsedData": [
+    {
+      "country": "Austria",
+      "id": "AT",
+      "value": 0.386,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Belgium",
+      "id": "BE",
+      "value": 0.408,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Bulgaria",
+      "id": "BG",
+      "value": 0.172,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Croatia",
+      "id": "HR",
+      "value": 0.19,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Cyprus",
+      "id": "CY",
+      "value": 0.071,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Czechia",
+      "id": "CZ",
+      "value": 0.444,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Denmark",
+      "id": "DK",
+      "value": 0.185,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Estonia",
+      "id": "EE",
+      "value": 0.047,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Finland",
+      "id": "FI",
+      "value": 0.178,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "France",
+      "id": "FR",
+      "value": 2.269,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Germany",
+      "id": "DE",
+      "value": 3.672,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Greece",
+      "id": "EL",
+      "value": 0.545,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Hungary",
+      "id": "HU",
+      "value": 0.355,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Ireland",
+      "id": "IE",
+      "value": 0.131,
+      "extraInfo": true,
+      "text_content": "Data from 2022"
+    },
+    {
+      "country": "Italy",
+      "id": "IT",
+      "value": 2.564,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Latvia",
+      "id": "LV",
+      "value": 0.06,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Lithuania",
+      "id": "LT",
+      "value": 0.108,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Luxembourg",
+      "id": "LU",
+      "value": 0.018,
+      "extraInfo": true,
+      "text_content": "Data from 2022"
+    },
+    {
+      "country": "Malta",
+      "id": "MT",
+      "value": 0.016,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Poland",
+      "id": "PL",
+      "value": 1.358,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Portugal",
+      "id": "PT",
+      "value": 0.3,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Romania",
+      "id": "RO",
+      "value": 0.417,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Slovak Republic",
+      "id": "SK",
+      "value": 0.228,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Slovenia",
+      "id": "SI",
+      "value": 0.082,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Spain",
+      "id": "ES",
+      "value": 1.565,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "Sweden",
+      "id": "SE",
+      "value": 0.37,
+      "extraInfo": false,
+      "text_content": ""
+    },
+    {
+      "country": "The Netherlands",
+      "id": "NL",
+      "value": 0.385,
+      "extraInfo": false,
+      "text_content": ""
+    }
+  ],
+  "datasetType": "values",
+  "datasetUnit": "fullNumbers",
+  "percentRounded": false,
+  "colourSchemeType": "sequential",
+  "colourScheme": "red",
+  "colourSchemeReverse": false,
+  "headlineAvailable": true,
+  "subheadlineAvailable": true,
+  "tooltipAvailable": true,
+  "scaleBarAvailable": true,
+  "overrideScaleValues": true,
+  "legendAvailable": true,
+  "textSourceAvailable": true,
+  "textSourceDescription": "Source",
+  "textSource": "European Directorate for the Quality of Medicines & Healthcare",
+  "textNoteAvailable": true,
+  "textNoteDescription": "Note",
+  "textNote": "This map represents the number of total donors (including first time donors) which donated whole blood or apheresis components for transfusion at least once a year. Meaning the same person could have donated more than once.",
+  "textDataAccessAvailable": true,
+  "linkDataAccessDescription": "Access the data",
+  "linkDataAccess": "https://freepub.edqm.eu/publications/PUBSD-280/detail",
+  "legend1": "No data available",
+  "legend1Color": "#E0E0E0",
+  "colorBarFirstValue": "0",
+  "colorBarLastValue": "3.7",
+  "customUnitLabelAvailable": true,
+  "customUnitLabel": "million whole blood units collected",
+  "tooltipExtraInfoLabel": "Click here",
+  "translate": {
+    "title": "Where is blood scarcity in the EU?",
+    "subtitle": "In 2023, around 16.5 million whole blood units were collected across the EU27, with Germany reporting the highest number at 3.7 million, followed by Italy, France and Spain. Among countries with larger populations, Bulgaria and Finland recorded some of the lowest figures, both below 0.2 million units.",
+    "textNoteDescription": "Note",
+    "textNote": "This map represents the number of total donors (including first time donors) which donated whole blood or apheresis components for transfusion at least once a year. Meaning the same person could have donated more than once.",
+    "textSourceDescription": "Source",
+    "textSource": "European Directorate for the Quality of Medicines & Healthcare",
+    "linkDataAccessDescription": "Access the data",
+    "legend1": "No data available",
+    "tooltipExtraInfoLabel": "Click here",
+    "extraInfo_IE": "Data from 2022",
+    "extraInfo_LU": "Data from 2022"
+  },
+  "clusters": [],
+  "colorScale": null,
+  "extraInfo_IE": "Data from 2022",
+  "extraInfo_LU": "Data from 2022"
 });
